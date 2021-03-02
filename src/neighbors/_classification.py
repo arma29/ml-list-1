f02e9e83a4048f9b9c2f95f75b90d6e2cae8c61b
Arnaldo Morais <arma@cin.ufpe.br>
2021-03-02 05:38:41 -0300

    Add adaptive implementation

--- src/neighbors/_classification.py
+++ src/neighbors/_classification.py
@@ -20,11 +20,27 @@ class Knn(BaseEstimator, ClassifierMixin):
         self.weights = weights
         self.__is_fitted = False
         self.__y_pred_func = self.__get_y_pred_func()
+        self.__epslon = 1e-10
 
     def fit(self, X, y):
         self.__fit_X = X
         self.__y = y
         self.__is_fitted = True
+        self.__radius = []
+        if(self.weights == 'adaptive'):
+            self.__initialize_radius_array()
+    
+    def __initialize_radius_array(self):
+        for i in range(len(self.__fit_X)):
+            minr = 999999
+            for j in range(len(self.__fit_X)):
+                if(i == j or self.__y[i] == self.__y[j]):
+                    continue
+
+                dist = np.linalg.norm(np.array(self.__fit_X[i]) - np.array(self.__fit_X[j]))
+                minr = min(minr, max(dist - self.__epslon, self.__epslon))
+
+            self.__radius.append(minr)
 
     def predict(self, X):
         if(not self.__is_fitted):
@@ -40,17 +56,24 @@ class Knn(BaseEstimator, ClassifierMixin):
             [[] for _ in range(len(transposed[0]))] for _ in range(len(transposed))]
         for i in range(len(transposed)):
             for j in range(len(transposed[i])):
-                training_examples[i][j] = [transposed[i][j], self.__y[j]]
+                dist = self.__get_dist(transposed,i,j)
+                training_examples[i][j] = [dist, self.__y[j]]
         return training_examples
+
+    def __get_dist(self, transposed,i,j):
+        if(self.weights == 'adaptive'):
+            return transposed[i][j] / (self.__radius[j])
+        else:
+            return transposed[i][j]
 
     def __get_k_nearests(self, training_examples):
         return [sorted(x, key=lambda y: y[0])[:self.n_neighbors] for x in training_examples]
 
     def __get_y_pred_func(self):
-        if(self.weights == 'uniform'):
-            return self.__get_y_pred_uniform
-        elif(self.weights == 'distance'):
+        if(self.weights == 'distance'):
             return self.__get_y_pred_distance
+        else:
+            return self.__get_y_pred_uniform
 
     def __get_y_pred_uniform(self, k_nearests):
         freqs = [[y[1] for y in x] for x in k_nearests]
@@ -67,7 +90,7 @@ class Knn(BaseEstimator, ClassifierMixin):
             for i in range(len(k_nearests)): # Foreach xq
                 summ = 0
                 for xk in k_nearests[i]: # Foreach k instance on xq
-                    wi = 1/(xk[0]**2 + 1e-10)
+                    wi = 1/(xk[0]**2 + self.__epslon)
                     summ += wi*self.__delta(v, xk[1])
                 v_arr[i].append(summ)
                 
